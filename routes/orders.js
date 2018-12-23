25d67e87bd7181ef82d500836df0834971617db5
RJS66 <rjs.github.com>
2018-12-23 18:54:07 -0500

    first commit

---
+++ routes/orders.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const orders = require('./../data/orders')
+
+// handle incoming requests to /users
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    orders: orders
+  })
+})
+
+module.exports = router
